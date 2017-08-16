4af422f01ae93d8af3aa61028c396e842f95686b
Markus Kahl <machisuji@gmail.com>
2017-08-16 23:45:37 +0100

    updated test area to include 2 hosts

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -9,113 +9,92 @@ $.fn.enterKey = function (fnc) {
     })
 }
 
-$("#host-start").click(function() {
-  var url = baseURL + "/session?keepAlive=keepAlive";
-  var info = $("#host-input").val();
+var scrollToBottom = function(textarea) {
+  textarea.scrollTop(textarea[0].scrollHeight);
+};
 
-  if (info.indexOf("/connect ") != -1 && info.indexOf(":") != -1) {
-    var i = info.substring(9).split(":");
-    var id = i[0];
-    var secret = i[1];
+var setupClient = function(n, sessionId) {
+  $("#client-" + n + "-connect").click(function() {
+    var button = $(this);
+    var client = new WebSocket(baseURL + "/session/" + sessionId + "?keepAlive=keepAlive&client=n" + n);
 
-    url = url + "&id=" + id + "&secret=" + secret;
+    client.onmessage = function(e) {
+      var message = e.data;
 
-    alert("Trying to take over session " + id);
-  }
+      if (message != "keepAlive") {
+        scrollToBottom($("#client-" + n + "-output").append(message + "\n"));
+      }
+      console.log("client " + n + " received message: " + message);
+    };
 
-  var socket = new WebSocket(url);
-  var scrollToBottom = function() {
-    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-  };
+    client.onopen = function(e) {
+      client.send("Hello from client " + n + "!");
+      button.attr("disabled", "disabled");
+    };
 
-  socket.onmessage = function(e) {
-    var response = e.data;
+    client.onclose = function(e) {
+      scrollToBottom($("#client-" + n + "-output").append("connection failed\n"));
+      button.removeAttr("disabled");
+    };
 
-    if (response != "keepAlive") {
-      $("#host-output").append(response + "</br>");
-      scrollToBottom();
+    $("#client-" + n + "-input").enterKey(function() {
+      var message = $(this).val();
+
+      client.send(message);
+      console.log("client " + n + " sent message: " + message);
+
+      $(this).val("");
+    });
+  }).removeAttr("disabled");
+};
+
+var setupHost = function(n) {
+  $("#host-" + n + "-start").click(function() {
+    var url = baseURL + "/session?keepAlive=keepAlive";
+    var info = $("#host-" + n + "-input").val();
+
+    if (info.indexOf("/connect ") != -1 && info.indexOf(":") != -1) {
+      var i = info.substring(9).split(":");
+      var id = i[0];
+      var secret = i[1];
+
+      url = url + "&id=" + id + "&secret=" + secret;
+
+      scrollToBottom($("#host-" + n + "-output").append("Trying to take over session " + id + "\n"));
     }
-    console.log("host received message: " + response);
 
-    if (response.startsWith("session:")) {
-      var sessionId = response.split(":")[1].trim();
+    var socket = new WebSocket(url);
 
-      $("#client-1-connect").click(function() {
-        var client = new WebSocket(baseURL + "/session/" + sessionId + "?keepAlive=keepAlive&client=n1");
+    socket.onmessage = function(e) {
+      var response = e.data;
 
-        client.onmessage = function(e) {
-          var message = e.data;
+      if (response != "keepAlive") {
+        scrollToBottom($("#host-" + n + "-output").append(response + "\n"));
+      }
+      console.log("host " + n + " received message: " + response);
 
-          if (message != "keepAlive") {
-            $("#client-1-output").append(message + "</br>");
-            scrollToBottom();
-          }
-          console.log("client 1 received message: " + message);
-        };
+      if (response.startsWith("session:")) {
+        var sessionId = response.split(":")[1].trim();
 
-        client.onopen = function(e) {
-          client.send("ClientId: 0x1000");
-        };
+        setupClient((n - 1) * 2 + 1, sessionId);
+        setupClient((n - 1) * 2 + 2, sessionId);
+      }
+    };
 
-        client.onclose = function(e) {
-          $("#client-1-output").append("connection failed");
-          scrollToBottom();
-        };
+    socket.onclose = function(e) {
+      scrollToBottom($("#host-" + n + "-output").append("connection failed\n"));
+    };
 
-        $("#client-1-input").enterKey(function() {
-          var message = $(this).val();
+    $("#host-" + n + "-input").enterKey(function() {
+      var message = $(this).val();
 
-          client.send(message);
-          console.log("client 1 sent message: " + message);
+      socket.send(message);
+      console.log("host " + n + " sent message: " + message);
 
-          $(this).val("");
-        });
-      });
-
-      $("#client-2-connect").click(function() {
-        var client2 = new WebSocket(baseURL + "/session/" + sessionId + "?keepAlive=keepAlive&client=n2");
-
-        client2.onmessage = function(e) {
-          var message = e.data;
-
-          if (message != "keepAlive") {
-            $("#client-2-output").append(message + "</br>");
-            scrollToBottom();
-          }
-          console.log("client 2 received message: " + message);
-        };
-
-        client2.onopen = function(e) {
-          client2.send("ClientId: 0x1001");
-        };
-
-        client2.onclose = function(e) {
-          $("#client-2-output").append("connection failed");
-          scrollToBottom();
-        };
-
-        $("#client-2-input").enterKey(function() {
-          var message = $(this).val();
-
-          client2.send(message);
-          console.log("client 2 sent message: " + message);
-
-          $(this).val("");
-        });
-      });
-    }
-  };
-
-  socket.onclose = function(e) {
-    $("#host-output").append("connection failed");
-  };
-
-  $("#host-input").enterKey(function() {
-    var message = $(this).val();
-
-    socket.send(message);
-    console.log("host sent message: " + message);
-
-    $(this).val("");
+      $(this).val("");
+    });
   });
-});
+};
+
+setupHost(1);
+setupHost(2);
